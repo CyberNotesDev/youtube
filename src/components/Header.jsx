c0a604f2b7c7260b87a291405b818b84657f0e95
Elijah Littell <elittell214@gmail.com>
2022-09-24 14:35:28 -0400

    Added imports and Header layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,39 @@
 import React from "react";
 import styles from "./../css/head.module.css";
 
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import VideoCallOutlinedIcon from "@mui/icons-material/VideoCallOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
+
+import Av from "./../img/steven-hand.jpg"
+import Youtube from "./../img/youtube.jpg";
+
 export function Header() {
   return (
     <header className={styles.header}>
-      <div>
-        <div>
-          <h3>Youtube</h3>
+      <nav className={styles.nav}>
+
+        <div className={styles.hamburger}>
+          <MenuIcon sx={{ fontSize: 35 }} />
+          <img className={styles.youtubeLogo} src={Youtube} alt="Youtube" />
         </div>
-        <div>
-          <p>Search</p>
+
+        <div className={styles.searchContainer}>
+          <input className={styles.search} type="search" placeholder="Search" />
+          <button className={styles.searchButton}>
+            <SearchIcon sx={{ fontSize: 25 }} />
+          </button>
         </div>
-        <div>
-          <p>Profile</p>
-        </div>
-      </div>
+
+        <Stack direction="row" alignItems="center" spacing={3}>
+          <VideoCallOutlinedIcon sx={{ fontSize: 30 }} />
+          <NotificationsOutlinedIcon  sx={{ fontSize: 30 }} />
+          <Avatar src={Av} alt="SpaceLess" sx={{ width: 35, height: 35 }}>Eli</Avatar>
+        </Stack>
+      </nav>
     </header>
   );
 }
